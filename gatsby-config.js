95bc2c2e6a9cb368d1cec114ca62ae670cff61d9
AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>
2021-03-07 00:45:59 +0530

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,21 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `AgilePlaya Portfolio`,
+    siteTitle: `AgilePlaya Portfolio`,
+    // Default title of the page
+    siteTitleAlt: `AgilePlaya's Portfolio Website`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `AgilePlaya's Portfolio Website`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://agileplaya.github.io`,
+    // Used for SEO
+    siteDescription: `Profile home page using NodeJS.`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `AgilePlaya`,
   },
   plugins: [
     {
